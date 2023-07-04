5381ccf528887abeb97821755541424357951d13
Abiel Kiflu <abiel.kiflu.weld@gmail.com>
2023-07-04 23:16:23 +0200

    3D viewer

---
+++ SerialPortVirtual/Form3D.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace SerialPortVirtual
+{
+    partial class Form3D
+    {
+
+        public string filePath { get; set; }
+
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+            // 
+            // Form3D
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(848, 508);
+            Name = "Form3D";
+            Text = "Form3D";
+            Load += Form3D_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
