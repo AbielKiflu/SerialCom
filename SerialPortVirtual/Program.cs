289031c8fd6f2da43b88e0e29f0045c05d69c3d1
Abiel Kiflu <abiel.kiflu.weld@gmail.com>
2023-07-04 08:35:13 +0200

    getport function param added

--- SerialPortVirtual/Program.cs
+++ SerialPortVirtual/Program.cs
@@ -1,12 +1,24 @@
-﻿namespace SerialPortVirtual
+﻿using System.IO.Ports;
+
+namespace SerialPortVirtual
 {
     internal class Program
     {
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Hi there");
+             
 
+            // display list of ports
+            Console.WriteLine(string.Join(", ",SerialPort.GetPortNames()));
+
+            SerialPort conn = Connection.getPort("COM1");
+            conn.Open();
+
+            conn.WriteLine("Hi there");
+    
+
+            conn.Close();
             Console.ReadKey();
 
         }
